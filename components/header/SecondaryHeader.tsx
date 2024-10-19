f2e6fa7bf0ff37548612c394962be94b658daaa7
nameishappy <105534032+nameishappy@users.noreply.github.com>
2024-10-19 18:27:58 +0530

    Revert "header"
    This reverts commit 8ff4665be14e43cfdd43715b60925a505c4e95be.

--- components/header/SecondaryHeader.tsx
+++ components/header/SecondaryHeader.tsx
@@ -14,14 +14,6 @@ const links = [
     label: "Get Started",
     href: "https://tally.so/r/wg49NK",
   },
-  {
-    label: "team",
-    href: "/team",
-  },
-  {
-    label: "use-cases",
-    href: "/use-cases",
-  },
 ];
 
 const SecondaryHeader = () => {
@@ -51,22 +43,6 @@ const SecondaryHeader = () => {
 
           {/* Desktop Menu */}
           <div className="hidden md:flex items-center space-x-5">
-            <Link
-              href={links[2].href}
-              className={`px-1 py-2 cursor-pointer ${
-                isVisible ? "text-[#333333]" : "text-white"
-              } rounded-md `}
-            >
-              Team
-            </Link>
-            <Link
-              href={links[3].href}
-              className={`px-1 py-2 cursor-pointer ${
-                isVisible ? "text-[#333333]" : "text-white"
-              } rounded-md `}
-            >
-              Use case
-            </Link>
             <Link
               href={links[0].href}
               className={`px-1 py-2 cursor-pointer  text-[#333333] rounded-md `}
